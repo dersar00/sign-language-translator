46f1dc71a2df82052b708ffae18ceb26ae03e99e
Valentin Mogish <dersar00@gmail.com>
2019-12-02 20:07:20 +0200

    Release instructions

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,6 +1,20 @@
+"""
+This class manages video filtering and display
+
+Author: Sufiyaan Nadeem
+Sources: 
+https://solarianprogrammer.com/2018/04/21/python-opencv-show-video-tkinter-window/
+https://docs.opencv.org/3.0-beta/modules/video/doc/motion_analysis_and_object_tracking.html?highlight=createbackgroundsubtractormog
+"""
 import cv2
 
 class VideoCapture:
+
+    """
+    Sets up Video Capturing using OpenCV and defines filters that can be used in other methods
+
+    :param video_source The Video Source number of the desired camera
+    """
     def __init__(self, video_source=0):
         # Open the video source
         self.vid = cv2.VideoCapture(video_source)
@@ -10,24 +24,21 @@ class VideoCapture:
         # Get video source width and height
         self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        self.fgbg=cv2.bgsegm.createBackgroundSubtractorMOG()
 
+    """
+    Reads the current frame, applies filters, and returns a frame
+    that Tkinter can display in RGB. 
+    * Currently also used to display filter results as I'm working towards background subtraction
 
-
+    :return current frame in RGB
+    """
     def get_frame(self):
         if self.vid.isOpened():
             ret, frame = self.vid.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-            self.fgmask=self.fgbg.apply(gray)
-            
-            self.fgmask= cv2.cvtColor(self.fgmask, cv2.COLOR_GRAY2RGB)
-            #cv2.imshow('frame',fgmask)
-            cv2.imshow("frame",self.fgmask)
-            #frame=self.backgroundSubstractMethod1.apply(frame)
+
             if ret:
-                # Return a boolean success flag and the current frame converted to BGR
-                return (ret, frame)#cv2.cvtColor(self.fgmask, cv2.COLOR_GRAY2GRAY))
+                # Return a boolean success flag and the current frame converted to RGB
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             else:
                 return (ret, None)
         else:
--- Window.py
+++ Window.py
@@ -1,11 +1,28 @@
+"""
+This class manages the window displayed on the user's screen.
+
+Author: Sufiyaan Nadeem
+Sources: 
+https://solarianprogrammer.com/2018/04/21/python-opencv-show-video-tkinter-window/
+http://effbot.org/tkinterbook/
+"""
+
 import tkinter
 import PIL.Image, PIL.ImageTk
 import time
 import tkinter.font as tkFont
 import numpy as np
 from VideoCapture import VideoCapture
+from BackgroundSubtract import BackgroundSubtract
 
-class App:
+class Window:
+
+    """
+    Sets up the User Interface of the Window; including buttons, fonts, titles, etc. 
+
+    :param window The Tkinter Window
+    :param video_source The Video Source
+    """
     def __init__(self, window, video_source=0):
         self.window = window
         self.customFont=tkFont.Font(family="Product Sans",size=14)
@@ -16,27 +33,29 @@ class App:
 
         self.screenHeight=self.window.winfo_screenheight()
         self.screenWidth=self.window.winfo_screenwidth()
-
-        
-
+     
         # open video source (by default this will try to open the computer webcam)
         self.vid = VideoCapture(self.video_source)
         #self.windowX=self.screenWidth/2-self.vid.width/2
         #self.windowY=self.screenHeight/2-self.vid.height/2
+        
+        #Type of filter than can be applied later on
+        self.vidProcessing=BackgroundSubtract()
 
         # Create a canvas that can fit the above video source size
         self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
         self.canvas.pack()
 
         # Button that lets the user take a snapshot
-        #self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot,font=self.customFont)
-        #self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
+        self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot,font=self.customFont)
+        self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
 
+        """Documentation Button
         self.loadimage = tkinter.PhotoImage(file="Images\documentation.png")
         self.roundedbutton =tkinter.Button(window, image=self.loadimage,command=self.snapshot)
         self.roundedbutton["bg"] = "white"
         self.roundedbutton["border"] = "0"
-        self.roundedbutton.pack(side="top")
+        self.roundedbutton.pack(side="top")"""
         #self.window.geometry("%dx%d+%d+%d"%(self.screenWidth,self.screenHeight,self.windowX,self.windowY))
         self.window.geometry("%dx%d+%d+%d"%(self.screenWidth,self.screenHeight,0,0))
 
@@ -45,7 +64,10 @@ class App:
         self.update()
 
         self.window.mainloop()
- 
+
+    """
+    Saves the current frame as an image
+    """
     def snapshot(self):
         # Get a frame from the video source
          ret, frame = self.vid.get_frame()
@@ -53,6 +75,9 @@ class App:
          if ret:
              cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
  
+    """
+    Displays the current frame in the window 
+    """
     def update(self):
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
@@ -60,7 +85,11 @@ class App:
         if ret:
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+        
+        subtractedBack=self.vidProcessing.subtractBack(frame)
+        gaussianBlurred=self.vidProcessing.gaussianBlur(frame)
 
+        self.vidProcessing.mask(frame,subtractedBack)        
         self.window.after(self.delay, self.update)
  
  
